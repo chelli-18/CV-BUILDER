a098a809db418d7a07fb15c1295d8c09a85e008e
arunach06 <72806925+arunach06@users.noreply.github.com>
2020-10-31 00:14:15 +0530

    Add files via upload

---
+++ selection.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="mobile-web-app-capable" content="yes" />
+    <link rel="stylesheet" href="./bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./bootstrap/js/bootstrap.min.js">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <link rel="stylesheet" href="selection.css">
+  </head>
+  <body>
+    <div class="container-sm">
+      <div class="d-block ml-auto mr-auto">
+        <div class="cv1 p-2 m-2 d-inline-block">
+          <img src="cv1.PNG" alt="">
+          <img src="cv1.PNG" alt="">
+          <a href="cv.php?mess=t1"><h3  class="text-center" class="text-center">template1</h3></a>
+
+        </div>
+        <div class="cv1 p-2 m-2 d-inline-block">
+          <img src="temp2.jpeg" alt="">
+          <a href="cv.php?mess=t2"><h3  class="text-center">template2</h3></a>
+
+        </div>
+        <div class="cv1 p-2 m-2 d-inline-block">
+          <img src="temp3.jpeg" alt="">
+          <a href="cv.php?mess=t3"><h3  class="text-center">template3</h3></a>
+        </div>
+        </div>
+      </div>
+    </div>
+  </body>
